19026702cdbe46d9f45dc4930980569f752b96d7
Agus-Coder <93748858+Agus-Coder@users.noreply.github.com>
2022-03-26 21:26:29 -0300

    Se agregan comentarios respecto a nodemon y final de leccion html

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log('hello there, im working in real time')
+
+//npm run dev con nodemon instalado me deja ver cambios en vivo de mi c[odigo ]. Con esto no hace falta que escriba
+//node app.js, sino que el modulo utilizado va a estar mirando los cambios que puedan suceder y actualizartelosolo
+
+//recordar pasos:
+
+//1 node i nodemon
+
+// 2 en json de nuestro proyecto modificar script de la siguiente manera:
+
+// "scripts": {
+//     "start": "node app.js",
+//     "dev": "nodemon app.js"
+//   },
+
+// 3 Ejecutar npm run dev y listo
